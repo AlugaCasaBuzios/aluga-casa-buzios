5ba92d23dae55f3805b0c0d0cec62b6c81f6eddd
Aluga Casa Buzios <contato@alugacasabuzios.com.br>
2026-07-28 22:48:15 -0300

    Atualiza Casa Conforto e adiciona novas fotos

--- app/data/properties.ts
+++ app/data/properties.ts
@@ -3,49 +3,31 @@ import type { Property } from "@/types/Property";
 export const properties: Property[] = [
   {
     id: "arete-top",
-
     title: "Aretê Top",
-
     neighborhood: "EDITAR BAIRRO",
-
     address: "Armação dos Búzios - RJ",
 
     guests: 0,
-
     bedrooms: 0,
-
     bathrooms: 0,
-
     beds: 0,
-
     suites: 0,
-
     area: 0,
-
     garage: 0,
 
     petFriendly: false,
-
     pool: false,
-
     barbecue: false,
-
     wifi: true,
-
     airConditioning: true,
-
     kitchen: true,
-
     washingMachine: true,
 
     beachDistance: "EDITAR DISTÂNCIA DA PRAIA",
-
     checkin: "15:00",
-
     checkout: "11:00",
 
     price: 0,
-
     cleaningFee: 0,
 
     image: "/images/properties/arete-top/1.jpg",
@@ -75,13 +57,10 @@ export const properties: Property[] = [
     ],
 
     airbnb: "",
-
     booking: "",
-
     whatsapp: "5524998288846",
 
     rating: 0,
-
     reviews: 0,
 
     keywords: [
@@ -94,52 +73,35 @@ export const properties: Property[] = [
 
   {
     id: "casa-da-margarida",
-
     title: "Casa da Margarida",
-
     neighborhood: "EDITAR BAIRRO",
-
     address: "Armação dos Búzios - RJ",
 
     guests: 0,
-
     bedrooms: 0,
-
     bathrooms: 0,
-
     beds: 0,
-
     suites: 0,
-
     area: 0,
-
     garage: 0,
 
     petFriendly: false,
-
     pool: false,
-
     barbecue: false,
-
     wifi: true,
-
     airConditioning: true,
-
     kitchen: true,
-
     washingMachine: true,
 
     beachDistance: "EDITAR DISTÂNCIA DA PRAIA",
-
     checkin: "15:00",
-
     checkout: "11:00",
 
     price: 0,
-
     cleaningFee: 0,
 
-    image: "/images/properties/casa-da-margarida/1.jpg",
+    image:
+      "/images/properties/casa-da-margarida/1.jpg",
 
     gallery: [
       "/images/properties/casa-da-margarida/1.jpg",
@@ -166,13 +128,10 @@ export const properties: Property[] = [
     ],
 
     airbnb: "",
-
     booking: "",
-
     whatsapp: "5524998288846",
 
     rating: 0,
-
     reviews: 0,
 
     keywords: [
@@ -185,52 +144,35 @@ export const properties: Property[] = [
 
   {
     id: "casa-doce-mar",
-
     title: "Casa Doce Mar",
-
     neighborhood: "EDITAR BAIRRO",
-
     address: "Armação dos Búzios - RJ",
 
     guests: 0,
-
     bedrooms: 0,
-
     bathrooms: 0,
-
     beds: 0,
-
     suites: 0,
-
     area: 0,
-
     garage: 0,
 
     petFriendly: false,
-
     pool: false,
-
     barbecue: false,
-
     wifi: true,
-
     airConditioning: true,
-
     kitchen: true,
-
     washingMachine: true,
 
     beachDistance: "EDITAR DISTÂNCIA DA PRAIA",
-
     checkin: "15:00",
-
     checkout: "11:00",
 
     price: 0,
-
     cleaningFee: 0,
 
-    image: "/images/properties/casa-doce-mar/1.jpg",
+    image:
+      "/images/properties/casa-doce-mar/1.jpg",
 
     gallery: [
       "/images/properties/casa-doce-mar/1.jpg",
@@ -257,13 +199,10 @@ export const properties: Property[] = [
     ],
 
     airbnb: "",
-
     booking: "",
-
     whatsapp: "5524998288846",
 
     rating: 0,
-
     reviews: 0,
 
     keywords: [
@@ -276,52 +215,35 @@ export const properties: Property[] = [
 
   {
     id: "casa-em-buzios",
-
     title: "Casa em Búzios",
-
     neighborhood: "EDITAR BAIRRO",
-
     address: "Armação dos Búzios - RJ",
 
     guests: 0,
-
     bedrooms: 0,
-
     bathrooms: 0,
-
     beds: 0,
-
     suites: 0,
-
     area: 0,
-
     garage: 0,
 
     petFriendly: false,
-
     pool: false,
-
     barbecue: false,
-
     wifi: true,
-
     airConditioning: true,
-
     kitchen: true,
-
     washingMachine: true,
 
     beachDistance: "EDITAR DISTÂNCIA DA PRAIA",
-
     checkin: "15:00",
-
     checkout: "11:00",
 
     price: 0,
-
     cleaningFee: 0,
 
-    image: "/images/properties/casa-em-buzios/1.jpg",
+    image:
+      "/images/properties/casa-em-buzios/1.jpg",
 
     gallery: [
       "/images/properties/casa-em-buzios/1.jpg",
@@ -348,13 +270,10 @@ export const properties: Property[] = [
     ],
 
     airbnb: "",
-
     booking: "",
-
     whatsapp: "5524998288846",
 
     rating: 0,
-
     reviews: 0,
 
     keywords: [
@@ -367,52 +286,35 @@ export const properties: Property[] = [
 
   {
     id: "casa-toriba",
-
     title: "Casa Toriba",
-
     neighborhood: "EDITAR BAIRRO",
-
     address: "Armação dos Búzios - RJ",
 
     guests: 0,
-
     bedrooms: 0,
-
     bathrooms: 0,
-
     beds: 0,
-
     suites: 0,
-
     area: 0,
-
     garage: 0,
 
     petFriendly: false,
-
     pool: false,
-
     barbecue: false,
-
     wifi: true,
-
     airConditioning: true,
-
     kitchen: true,
-
     washingMachine: true,
 
     beachDistance: "EDITAR DISTÂNCIA DA PRAIA",
-
     checkin: "15:00",
-
     checkout: "11:00",
 
     price: 0,
-
     cleaningFee: 0,
 
-    image: "/images/properties/casa-toriba/1.jpg",
+    image:
+      "/images/properties/casa-toriba/1.jpg",
 
     gallery: [
       "/images/properties/casa-toriba/1.jpg",
@@ -439,13 +341,10 @@ export const properties: Property[] = [
     ],
 
     airbnb: "",
-
     booking: "",
-
     whatsapp: "5524998288846",
 
     rating: 0,
-
     reviews: 0,
 
     keywords: [
@@ -458,52 +357,35 @@ export const properties: Property[] = [
 
   {
     id: "centro-top",
-
     title: "Centro Top",
-
     neighborhood: "Centro",
-
     address: "Centro - Armação dos Búzios",
 
     guests: 6,
-
     bedrooms: 2,
-
     bathrooms: 2,
-
     beds: 4,
-
     suites: 1,
-
     area: 120,
-
     garage: 2,
 
     petFriendly: true,
-
     pool: false,
-
     barbecue: true,
-
     wifi: true,
-
     airConditioning: true,
-
     kitchen: true,
-
     washingMachine: true,
 
     beachDistance: "500 m da Rua das Pedras",
-
     checkin: "15:00",
-
     checkout: "11:00",
 
     price: 650,
-
     cleaningFee: 180,
 
-    image: "/images/properties/centro-top/1.jpg",
+    image:
+      "/images/properties/centro-top/1.jpg",
 
     gallery: [
       "/images/properties/centro-top/1.jpg",
@@ -536,18 +418,17 @@ export const properties: Property[] = [
       "Proibido fumar dentro da casa",
     ],
 
-    airbnb: "https://airbnb.com.br/h/centro-top",
+    airbnb:
+      "https://airbnb.com.br/h/centro-top",
 
     booking: "",
 
     whatsapp: "5524998288846",
 
     rating: 4.96,
-
     reviews: 147,
 
     latitude: -22.7466,
-
     longitude: -41.8813,
 
     keywords: [
@@ -562,143 +443,163 @@ export const properties: Property[] = [
 
   {
     id: "conforto",
+    title: "Casa Conforto no Condomínio Aretê",
+    neighborhood: "Condomínio Aretê",
 
-    title: "Conforto",
+    address:
+      "Condomínio Aretê, Armação dos Búzios - RJ",
 
-    neighborhood: "EDITAR BAIRRO",
-
-    address: "Armação dos Búzios - RJ",
-
-    guests: 0,
-
-    bedrooms: 0,
-
-    bathrooms: 0,
-
-    beds: 0,
-
-    suites: 0,
+    guests: 8,
+    bedrooms: 4,
+    bathrooms: 4.5,
+    beds: 5,
+    suites: 4,
 
     area: 0,
-
-    garage: 0,
+    garage: 4,
 
     petFriendly: false,
-
-    pool: false,
-
-    barbecue: false,
-
+    pool: true,
+    barbecue: true,
     wifi: true,
-
     airConditioning: true,
-
     kitchen: true,
-
     washingMachine: true,
 
-    beachDistance: "EDITAR DISTÂNCIA DA PRAIA",
+    beachDistance:
+      "7,5 km da Praia de Geribá",
 
-    checkin: "15:00",
-
+    checkin: "16:00",
     checkout: "11:00",
 
-    price: 0,
+    price: 1200,
+    cleaningFee: 300,
 
-    cleaningFee: 0,
-
-    image: "/images/properties/conforto/1.jpg",
+    image:
+      "/images/properties/conforto/airbnb-1.png",
 
     gallery: [
-      "/images/properties/conforto/1.jpg",
+      "/images/properties/conforto/airbnb-1.png",
+      "/images/properties/conforto/airbnb-2.png",
+      "/images/properties/conforto/airbnb-3.jpg",
+      "/images/properties/conforto/airbnb-4.png",
+      "/images/properties/conforto/airbnb-5.png",
     ],
 
     description:
-      "EDITAR: escreva aqui uma descrição completa da propriedade Conforto.",
+      "Relaxe em uma casa completa, elegante e tranquila no Condomínio Aretê. O imóvel acomoda até 8 hóspedes e possui 4 suítes climatizadas, lavabo, sala ampla, cozinha equipada, Wi-Fi, TV, área gourmet, piscina privativa em frente ao canal, garagem para 4 carros e uma vista deslumbrante para o pôr do sol. A casa também oferece uma vaga exclusiva para lancha em frente à piscina. Todos os ambientes são de uso exclusivo dos hóspedes. O condomínio conta com piscinas para adultos e crianças, sauna, playground, quadra de basquete, pista de ciclismo e acesso, mediante regras e eventuais taxas, ao Aretê Sports Club, Aretê Beach Club e Aretê Golf Club.",
 
     amenities: [
-      "Wi-Fi",
-      "Ar-condicionado",
-      "Cozinha completa",
+      "Piscina privativa em frente ao canal",
+      "Vaga exclusiva para lancha",
+      "Área gourmet",
+      "Churrasqueira",
+      "Vista para o pôr do sol",
+      "4 suítes climatizadas",
+      "1 lavabo",
+      "Sala ampla",
+      "Cozinha equipada",
       "Máquina de lavar",
-      "Smart TV",
-      "Garagem",
+      "Wi-Fi",
+      "TV",
+      "Roupa de cama e banho",
+      "Garagem para 4 carros",
+      "Piscina adulta no condomínio",
+      "Piscina infantil com salva-vidas",
+      "Sauna no condomínio",
+      "Playground",
+      "Quadra de basquete",
+      "Pista de ciclismo com 10,1 km",
+      "Acesso ao Aretê Sports Club",
+      "Acesso ao Aretê Beach Club",
+      "Acesso ao Aretê Golf Club",
     ],
 
     rules: [
-      "Check-in após 15h",
-      "Check-out até 11h",
-      "Não é permitido festas",
-      "Proibido fumar dentro da casa",
-      "Animais mediante consulta",
+      "A casa acomoda no máximo 8 hóspedes",
+      "A casa é de uso exclusivo dos hóspedes",
+      "Check-in a partir das 16h",
+      "Check-out até as 11h",
+      "Alterações no check-in ou check-out dependem de disponibilidade",
+      "A alteração de horário pode ter taxa de R$ 100 por hora",
+      "Nos finais de semana, saída após as 12h pode gerar cobrança de meia diária",
+      "Informar corretamente adultos, crianças e bebês na reserva",
+      "Não ultrapassar o número de hóspedes informado na reserva",
+      "Hóspedes excedentes podem gerar multa de R$ 200 por dia ou cancelamento da reserva",
+      "Enviar documento com foto de todos os hóspedes após a reserva",
+      "Enviar as placas dos veículos para autorização na portaria",
+      "Visitantes devem ser informados previamente ao anfitrião",
+      "Respeitar o horário de silêncio das 22h às 8h",
+      "Crianças devem permanecer supervisionadas na piscina",
+      "Não sentar molhado nos sofás",
+      "Não jogar papel higiênico, lenço umedecido ou fio dental no vaso sanitário",
+      "Deixar a louça lavada antes do check-out",
+      "Retirar os lixos e colocá-los na lixeira em frente à casa",
+      "Combinar o horário do check-out com o anfitrião",
+      "Aguardar a chegada do responsável antes de deixar o imóvel",
+      "A casa possui câmeras de segurança somente nas áreas externas",
+      "Roupa de cama e banho é fornecida sem troca durante a hospedagem",
+      "Trocas de roupa de cama e banho possuem taxa de lavanderia",
+      "Clubes, quadras e campo de golfe podem cobrar taxas próprias",
     ],
 
-    airbnb: "",
+    airbnb:
+      "https://www.airbnb.com.br/h/arete01",
 
     booking: "",
 
     whatsapp: "5524998288846",
 
     rating: 0,
-
     reviews: 0,
 
     keywords: [
-      "Conforto",
-      "Casa em Búzios",
-      "Temporada em Búzios",
-      "Hospedagem em Búzios",
+      "Casa Conforto Búzios",
+      "Casa no Condomínio Aretê",
+      "Casa com piscina em Búzios",
+      "Casa com vaga para lancha em Búzios",
+      "Casa de temporada para 8 pessoas",
+      "Casa com 4 suítes em Búzios",
+      "Hospedagem no Aretê Búzios",
+      "Aluguel de temporada em Búzios",
     ],
   },
 
   {
     id: "flat-tartaruga",
-
     title: "Flat Tartaruga",
-
     neighborhood: "Tartaruga",
 
-    address: "Tartaruga - Armação dos Búzios",
+    address:
+      "Tartaruga - Armação dos Búzios",
 
     guests: 0,
-
     bedrooms: 0,
-
     bathrooms: 0,
-
     beds: 0,
-
     suites: 0,
-
     area: 0,
-
     garage: 0,
 
     petFriendly: false,
-
     pool: false,
-
     barbecue: false,
-
     wifi: true,
-
     airConditioning: true,
-
     kitchen: true,
-
     washingMachine: true,
 
-    beachDistance: "EDITAR DISTÂNCIA DA PRAIA",
+    beachDistance:
+      "EDITAR DISTÂNCIA DA PRAIA",
 
     checkin: "15:00",
-
     checkout: "11:00",
 
     price: 0,
-
     cleaningFee: 0,
 
-    image: "/images/properties/flat-tartaruga/1.jpg",
+    image:
+      "/images/properties/flat-tartaruga/1.jpg",
 
     gallery: [
       "/images/properties/flat-tartaruga/1.jpg",
@@ -725,13 +626,10 @@ export const properties: Property[] = [
     ],
 
     airbnb: "",
-
     booking: "",
-
     whatsapp: "5524998288846",
 
     rating: 0,
-
     reviews: 0,
 
     keywords: [
---
+++ baixar_fotos_airbnb.ps1
@@ -0,0 +1,127 @@
+﻿# Baixa as 5 fotos públicas principais de cada anúncio do Airbnb.
+# Execute este arquivo dentro da pasta raiz do projeto:
+# C:\Users\Notebook-Acer-3\OneDrive\Documentos\GitHub\aluga-casa-buzios
+#
+# As imagens serão salvas em:
+# public\images\airbnb-import\<nome-do-imovel>\
+#
+# O script NÃO substitui as fotos atuais do site.
+
+$ErrorActionPreference = "Stop"
+
+if (-not (Test-Path ".\package.json")) {
+    Write-Host ""
+    Write-Host "ERRO: execute este script dentro da pasta raiz do projeto." -ForegroundColor Red
+    Write-Host "A pasta atual precisa conter o arquivo package.json." -ForegroundColor Yellow
+    exit 1
+}
+
+$headers = @{
+    "User-Agent" = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 Chrome/150 Safari/537.36"
+    "Accept"     = "image/avif,image/webp,image/apng,image/svg+xml,image/*,*/*;q=0.8"
+    "Referer"    = "https://www.airbnb.com.br/"
+}
+
+$imoveis = @{
+    "casa-toriba" = @(
+        @{ Arquivo = "1.png"; URL = "https://a0.muscache.com/im/pictures/hosting/Hosting-1671794373671107565/original/a877d226-195c-4b81-8918-023d6b4481a5.png" },
+        @{ Arquivo = "2.png"; URL = "https://a0.muscache.com/im/pictures/hosting/Hosting-1671794373671107565/original/686fc681-bd19-42b8-8794-cf3bad24b7a8.png" },
+        @{ Arquivo = "3.jpg"; URL = "https://a0.muscache.com/im/pictures/hosting/Hosting-1671794373671107565/original/d48b7b84-2a18-4709-a349-7e8c71a74cc5.jpeg" },
+        @{ Arquivo = "4.jpg"; URL = "https://a0.muscache.com/im/pictures/hosting/Hosting-1671794373671107565/original/b80d1223-6ec4-4c11-8190-c79928d433cc.jpeg" },
+        @{ Arquivo = "5.jpg"; URL = "https://a0.muscache.com/im/pictures/hosting/Hosting-1671794373671107565/original/647575bc-ed21-4b44-bcc6-130293d25960.jpeg" }
+    )
+
+    "casa-da-margarida" = @(
+        @{ Arquivo = "1.jpg"; URL = "https://a0.muscache.com/im/pictures/hosting/Hosting-33396295/original/6d9aab5a-1e59-4cbd-a5bf-1427eb02d745.jpeg" },
+        @{ Arquivo = "2.jpg"; URL = "https://a0.muscache.com/im/pictures/hosting/Hosting-33396295/original/340b49bd-5785-4675-b538-982f923e662b.jpeg" },
+        @{ Arquivo = "3.jpg"; URL = "https://a0.muscache.com/im/pictures/hosting/Hosting-33396295/original/acf70ff4-1394-4fa8-940c-283675355a66.jpeg" },
+        @{ Arquivo = "4.jpg"; URL = "https://a0.muscache.com/im/pictures/hosting/Hosting-33396295/original/06002e85-de65-49ca-af94-3d24a3d8045c.jpeg" },
+        @{ Arquivo = "5.jpg"; URL = "https://a0.muscache.com/im/pictures/hosting/Hosting-33396295/original/3ae270e0-df8a-4e2c-bc0a-2193a6babc1e.jpeg" }
+    )
+
+    "arete-top" = @(
+        @{ Arquivo = "1.jpg"; URL = "https://a0.muscache.com/im/pictures/hosting/Hosting-1572511786198194351/original/6ef3ad55-553e-48d7-b250-1572b8292956.jpeg" },
+        @{ Arquivo = "2.jpg"; URL = "https://a0.muscache.com/im/pictures/hosting/Hosting-1572511786198194351/original/c0c3aa8a-b691-42aa-a3bd-93c2be6c7099.jpeg" },
+        @{ Arquivo = "3.jpg"; URL = "https://a0.muscache.com/im/pictures/hosting/Hosting-1572511786198194351/original/57a7094d-0be7-4520-a7c9-e41de0691448.jpeg" },
+        @{ Arquivo = "4.jpg"; URL = "https://a0.muscache.com/im/pictures/hosting/Hosting-1572511786198194351/original/3731b0cd-8269-485d-a08a-6b7879f7ba35.jpeg" },
+        @{ Arquivo = "5.jpg"; URL = "https://a0.muscache.com/im/pictures/hosting/Hosting-1572511786198194351/original/6a67cfd5-96c9-46a6-a3ff-3bcbc600e51a.jpeg" }
+    )
+
+    "casa-doce-mar" = @(
+        @{ Arquivo = "1.png"; URL = "https://a0.muscache.com/im/pictures/hosting/Hosting-1573499649457752998/original/8d2767a2-7286-43e9-9f8b-db3611c320d3.png" },
+        @{ Arquivo = "2.png"; URL = "https://a0.muscache.com/im/pictures/hosting/Hosting-1573499649457752998/original/99b789d1-382f-4f6e-8971-77934ce1f7eb.png" },
+        @{ Arquivo = "3.jpg"; URL = "https://a0.muscache.com/im/pictures/hosting/Hosting-1573499649457752998/original/06c0dfd9-f708-4108-8105-79188ecbad0b.jpeg" },
+        @{ Arquivo = "4.jpg"; URL = "https://a0.muscache.com/im/pictures/hosting/Hosting-1573499649457752998/original/3a85a6eb-5ba6-458e-a58e-59da7e4e0b05.jpeg" },
+        @{ Arquivo = "5.jpg"; URL = "https://a0.muscache.com/im/pictures/hosting/Hosting-1573499649457752998/original/3593a4c5-f025-4eb1-9211-76225958f1ab.jpeg" }
+    )
+
+    "casa-em-buzios" = @(
+        @{ Arquivo = "1.jpg"; URL = "https://a0.muscache.com/im/pictures/airflow/Hosting-45951143/original/e24b8468-0fec-4809-a03d-1fd84e8e4c21.jpg" },
+        @{ Arquivo = "2.jpg"; URL = "https://a0.muscache.com/im/pictures/hosting/Hosting-45951143/original/392adb9a-8dfe-4de9-93b1-579ff9b52dba.jpeg" },
+        @{ Arquivo = "3.jpg"; URL = "https://a0.muscache.com/im/pictures/airflow/Hosting-45951143/original/6a938480-4c4d-45b7-b0df-3a3bf70753f8.jpg" },
+        @{ Arquivo = "4.png"; URL = "https://a0.muscache.com/im/pictures/hosting/Hosting-45951143/original/c2451fa5-f82a-4047-8fe7-111a77d3b2f5.png" },
+        @{ Arquivo = "5.jpg"; URL = "https://a0.muscache.com/im/pictures/airflow/Hosting-45951143/original/7d30dddb-8c21-446a-a6f5-a03d27f87212.jpg" }
+    )
+
+    "conforto" = @(
+        @{ Arquivo = "1.png"; URL = "https://a0.muscache.com/im/pictures/hosting/Hosting-1159472082721553654/original/4b3a8e84-9346-453f-b6b5-8db04ff7ddab.png" },
+        @{ Arquivo = "2.png"; URL = "https://a0.muscache.com/im/pictures/hosting/Hosting-1159472082721553654/original/39fe8f54-e3d8-4ffd-8d11-c27e18d7a690.png" },
+        @{ Arquivo = "3.jpg"; URL = "https://a0.muscache.com/im/pictures/miso/Hosting-1159472082721553654/original/b4d648d5-0704-4847-9c86-2d29a52dfd43.jpeg" },
+        @{ Arquivo = "4.png"; URL = "https://a0.muscache.com/im/pictures/hosting/Hosting-1159472082721553654/original/6679adac-deba-4648-a54e-e24c953beb55.png" },
+        @{ Arquivo = "5.png"; URL = "https://a0.muscache.com/im/pictures/hosting/Hosting-1159472082721553654/original/6646806b-7329-49ce-9d6f-6bb7262523b3.png" }
+    )
+}
+
+$base = Join-Path (Get-Location) "public\images\airbnb-import"
+New-Item -ItemType Directory -Force -Path $base | Out-Null
+
+$total = 0
+$sucesso = 0
+$falhas = 0
+
+foreach ($nome in $imoveis.Keys) {
+    $pasta = Join-Path $base $nome
+    New-Item -ItemType Directory -Force -Path $pasta | Out-Null
+
+    Write-Host ""
+    Write-Host "Baixando fotos de: $nome" -ForegroundColor Cyan
+
+    foreach ($foto in $imoveis[$nome]) {
+        $total++
+        $destino = Join-Path $pasta $foto.Arquivo
+
+        try {
+            Invoke-WebRequest `
+                -Uri $foto.URL `
+                -OutFile $destino `
+                -Headers $headers `
+                -MaximumRedirection 10
+
+            $arquivo = Get-Item $destino
+
+            if ($arquivo.Length -lt 1000) {
+                throw "O arquivo baixado está vazio ou incompleto."
+            }
+
+            $sucesso++
+            Write-Host "  OK  $($foto.Arquivo)  $([math]::Round($arquivo.Length / 1KB)) KB" -ForegroundColor Green
+        }
+        catch {
+            $falhas++
+            Write-Host "  ERRO  $($foto.Arquivo): $($_.Exception.Message)" -ForegroundColor Red
+
+            if (Test-Path $destino) {
+                Remove-Item $destino -Force
+            }
+        }
+    }
+}
+
+Write-Host ""
+Write-Host "Download concluído." -ForegroundColor Cyan
+Write-Host "Total: $total | Sucesso: $sucesso | Falhas: $falhas"
+Write-Host ""
+Write-Host "As fotos estão em:" -ForegroundColor Yellow
+Write-Host $base
+Write-Host ""
+Write-Host "Revise as imagens antes de substituir as fotos atuais do site." -ForegroundColor Yellow
